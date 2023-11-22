7de384efc417a1164571dbcc1af58c86f2a657ab
omaran bazna <omran4d@gmail.com>
2023-11-21 23:49:18 -0500

    Make availabilities form work

--- App/models/availabilities.js
+++ App/models/availabilities.js
@@ -12,8 +12,77 @@ let db = new sqlite3.Database(dbPath, (err) => {
   console.log("Connected to the SQLite database.");
 });
 
-function renderPage(req, res) {
-  res.render("availabilities");
+// Function to get professor information based on ProfessorID
+const getProfessorInfo = (professorId) =>
+  new Promise((resolve, reject) => {
+    let sql = "SELECT Fname, Lname FROM professor WHERE ProfessorID = ?";
+    db.all(sql, [professorId], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows[0].Fname + " " + rows[0].Lname);
+      }
+    });
+  });
+
+// Function to get course information based on CourseID
+const getCourseInfo = (courseId) =>
+  new Promise((resolve, reject) => {
+    let sql = "SELECT Course_Name, Course_Code FROM course WHERE CourseID = ?";
+    db.all(sql, [courseId], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows[0].Course_Name + " " + rows[0].Course_Code);
+      }
+    });
+  });
+let result = [];
+const renderPage = (req, res) => {
+  let sql = "SELECT * FROM availability";
+
+  db.each(
+    sql,
+    async (err, row) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        try {
+          // Fetch professor and course information asynchronously
+          let professorInfo = await getProfessorInfo(row.Prof_ID);
+          let courseInfo = await getCourseInfo(row.Cour_ID);
+
+          // Update the row with professor and course information
+          row.Prof_ID = professorInfo;
+          row.Cour_ID = courseInfo;
+
+          // Add the updated row to the result array
+          result.push(row);
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+    },
+    () => {
+      AddProfessors(req, res);
+    }
+  );
+};
+
+function AddProfessors(req, res) {
+  let sql = "select ProfessorID,Fname,Lname from professor";
+  db.all(sql, (err, prof) => {
+    let sql2 = "select  CourseID,Course_Name,Course_Code from course";
+    db.all(sql2, (err, course) => {
+      console.log(prof);
+      res.render("availabilities", {
+        rows: result,
+        course: course,
+        prof: prof,
+      });
+      result = [];
+    });
+  });
 }
 
 function addAvailability(req, res) {
@@ -25,7 +94,7 @@ function addAvailability(req, res) {
     time,
     day,
   } = req.body;
-  console.log(slots);
+
   let element = slots.find(
     (item) => item.day.indexOf(day) > -1 && item.time.indexOf(time) > -1
   );
--- App/models/database/test.sql
+++ App/models/database/test.sql
@@ -18,6 +18,9 @@ CREATE TABLE  course (
     Course_Duration TEXT NOT NULL,
     Course_Year INTEGER NOT NULL
   );
+
+
+
 CREATE TABLE availability (
     Ava_ID INTEGER PRIMARY KEY AUTOINCREMENT,
     Availability_Duration TEXT NOT NULL,
@@ -32,5 +35,22 @@ CREATE TABLE Time_Slot (
       Start_Time DATETIME NOT NULL
   )
 
- ---sqlite3 , sql
- --sql 
+INSERT INTO course (Major, Course_Name, Course_Code, Course_Type, Elective, Course_Iteration_Weekly, Course_Duration, Course_Year) VALUES ('EE', 'MATH (mathematics)', '1420-02', 'In Person', 0, 1, '10:00-11:00', 1);
+INSERT INTO course (Major, Course_Name, Course_Code, Course_Type, Elective, Course_Iteration_Weekly, Course_Duration, Course_Year) VALUES ('EE', 'MATH (mathematics)', '1420-03', 'In Person', 0, 1, '6:30-8:30', 1);
+INSERT INTO course (Major, Course_Name, Course_Code, Course_Type, Elective, Course_Iteration_Weekly, Course_Duration, Course_Year) VALUES ('EE', 'PHY (Physics)', '1610-01', 'In Person', 0, 1, '2:00-5:00', 1);
+INSERT INTO course (Major, Course_Name, Course_Code, Course_Type, Elective, Course_Iteration_Weekly, Course_Duration, Course_Year) VALUES ('EE', 'ELEE', '5350-01', 'In Person', 0, 1, '5:15-6:30', 1);
+
+INSERT INTO professor (Fname, Lname, Professor_Type, Seniority)
+VALUES ('Shadi', 'Bani Taan', 'Full-time', 7);
+
+INSERT INTO professor (Fname, Lname, Professor_Type, Seniority)
+VALUES ('Kathy', 'Zhong', 'Full-time', 5);
+
+INSERT INTO professor (Fname, Lname, Professor_Type, Seniority)
+VALUES ('Mariam', 'Faied', 'Full-time', 8);
+
+INSERT INTO professor (Fname, Lname, Professor_Type, Seniority)
+VALUES ('Jonathan', 'Weaver', 'adjunct', 2);
+
+INSERT INTO professor (Fname, Lname, Professor_Type, Seniority)
+VALUES ('James', 'Lynch', 'adjunct', 1);
